b8c095443062b5e1158c13c7f9b37f8c59539a3f
David Simon <me@davidsimon.ch>
2018-06-01 14:34:52 +0200

    [Chore] Whitespace / Comments

--- contracts/Satelite2.sol
+++ contracts/Satelite2.sol
@@ -5,13 +5,13 @@ import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Satelite2 is Ownable {
     using SafeMath for uint256;
-    event payed(uint256 i,address indexed to, uint256 amount);
+    event payed(uint256 i, address indexed to, uint256 amount);
 
-    uint256 public constant dezentrumPart = 15; 
+    uint256 public constant dezentrumPart = 15;
     uint256 public constant satelitePart = 3;
 
     address dezentrumW; //=decentrum wallet
-   
+
     Proposal[] proposals;
 
     uint256 claimTime;
@@ -27,17 +27,17 @@ contract Satelite2 is Ownable {
     }
 
     function Satelite2() public {
-        owner = msg.sender;
+        owner = msg.sender; // Potentially hardcode this to dezentrum multisig
     }
 
     modifier isNotClaimed() {
-        require(claimTime<block.timestamp-60*60*30);
+        require(claimTime < block.timestamp-60*60*30); // require there is not running claim
         _;
     }
-    
+
     modifier isClaimed() {
-        require(claimTime>=block.timestamp-60*60*30);
-        require(claimer != 0x0);
+        require(claimTime >= block.timestamp-60*60*30); // require it has not expired yet
+        require(claimer != 0x0); // require that there *is* actual claimer
         _;
     }
 
@@ -49,15 +49,15 @@ contract Satelite2 is Ownable {
         return nounce;
     }
 
-    function getNumberofProposals() view public returns(uint256) {
+    function getNumberofProposals() view public returns (uint256) {
         return proposals.length;
     }
 
-    function getProposals(i) view public returns(uint256,uint256 ,uint256 ,bool ) {
-        return (proposals[i].lat, proposals[i].lon, proposals[i].balance,proposals[i].finished);
+    function getProposal(i) view public returns (uint256, uint256, uint256, bool) {
+        return (proposals[i].lat, proposals[i].lon, proposals[i].balance, proposals[i].finished);
     }
 
-    function claim(uint256 _i,uint256 _nounce) isNotClaimed public returns(bool) {
+    function claim(uint256 _i, uint256 _nonce) isNotClaimed public returns (bool) {
         require(msg.sender != 0x0);
         claimer = msg.sender;
         nounce = _nounce;
@@ -78,7 +78,7 @@ contract Satelite2 is Ownable {
         return true;
     }
 
-    function addProposal (uint256 _lat, uint256 _lon) public returns(uint256)
+    function addProposal (int256 _lat, int256 _lon) public returns (uint256)
     {
         var newProposal = new Proposal();
         newProposal.lat = _lat;
